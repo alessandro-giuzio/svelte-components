24cf89ea2a78f80b0b5f91d718a279c8a672baa4
alessandro-giuzio <giuzio@icloud.com>
2022-03-11 13:19:32 +0100

    search

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,27 @@
 <script>
-    import Toggle from "$lib/Toggle.svelte";
-    import BetterAccordion from "$lib/BetterAccordion.svelte";
+   import SearchFilter from "$lib/SearchFilter.svelte";
     let isToggled = false;
+    let items = [
+        'Alessandro',
+        'Oihana',
+        'Luca',
+        'Maria',
+        'Haizea',
+        'rosarito',
+    ]
 </script>
 
 <h1>Alessandro's Components</h1>
-<BetterAccordion isOpen={true} buttonText = "Do I need it?" >
+
+<SearchFilter {items} />
+<!-- <BetterAccordion isOpen={true} buttonText = "Do I need it?" >
 <div>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam illum fugit tenetur deserunt nemo minus dolorum, quaerat ad quae. Aperiam doloremque veniam in quisquam laudantium.</p>
 </div>
-</BetterAccordion>
-<Toggle bind:isToggled label="Beta User" />
+</BetterAccordion> -->
+<!-- <Toggle bind:isToggled label="Beta User" />
 <Toggle bind:isToggled label="Beta User" style="--toggle-background-color:red"/>
-
+ -->
 {#if isToggled}
 <h1>I'm Toggled!</h1>
 {/if}
